9c698b65537306aaaf222a7cc0c1dccb268fa26a
vineet <vineet.agrawal100@gmail.com>
2019-07-25 10:01:04 +0530

    endpoint, trackByName @query

---
+++ README.md
@@ -0,0 +1,3 @@
+**Spring Boot Demo Application**
+3. Add an endpoint to search trackByName. Understand @Query and parameter passing to
+@Query
---
+++ src/main/java/com/stackroute/service/TrackService.java
@@ -0,0 +1,22 @@
+package com.stackroute.service;
+
+import com.stackroute.domain.Track;
+
+import java.util.List;
+
+public interface TrackService {
+
+    public Track saveTrack(Track track);
+
+    public Track getTrackById(int id);
+
+    public void deleteTrack(int id);
+
+    public List<Track> getAllTracks();
+
+    public Track updateTrack(int id,String comment);
+    List<Track> getByTrackName(String name);
+    List<Track> getTrackByNameSortByName(String name);
+
+
+}
