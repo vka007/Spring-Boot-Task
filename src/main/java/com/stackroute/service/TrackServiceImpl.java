0c1e9617e7758e0a030851a9554837d49c949198
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-08-07 10:49:40 +0530

    Private scope added

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 @Service
 public class TrackServiceImpl implements TrackService {
 
-    TrackRepository trackRepository;
+    private TrackRepository trackRepository;
 
     @Autowired
     public TrackServiceImpl(TrackRepository trackRepository) {
