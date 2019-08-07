c6210542d3b3081c3bb704c735f9da58664e2a01
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-08-07 10:43:18 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -9,9 +9,9 @@ import javax.persistence.Id;
 public class Track {
 
     @Id
-    int id;
-    String name;
-    String comment;
+    private int id;
+    private String name;
+    private String comment;
 
     public Track() {
     }
