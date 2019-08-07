457c42dbf2478bdaada0ec0a7d857ef8faba7c6a
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-08-07 11:38:23 +0530

    Update TrackServiceImpl.java

--- src/main/java/com/stackroute/service/TrackServiceImpl.java
+++ src/main/java/com/stackroute/service/TrackServiceImpl.java
@@ -3,7 +3,6 @@ package com.stackroute.service;
 import com.stackroute.domain.Track;
 import com.stackroute.repository.TrackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -31,27 +30,21 @@ public class TrackServiceImpl implements TrackService {
 
     @Override
     public Track getTrackById(int id) {
-
         Track track = trackRepository.findById(id).get();
         return track;
-
     }
 
     @Override
     public void deleteTrack(int id) {
-
         trackRepository.delete(getTrackById(id));
-
     }
 
     @Override
     public Track updateTrack(int id,String comment) {
-
         Optional<Track> track = trackRepository.findById(id);
         Track track1 = track.get();
         track1.setComment(comment);
         Track savedTrack = trackRepository.save(track1);
         return savedTrack;
-
     }
 }
