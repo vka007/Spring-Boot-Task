6f2652b56ec29da1109d6b8a7183e2ded5bc2ac2
Vineet Kumar Agrawal <31213238+vka007@users.noreply.github.com>
2019-08-07 11:53:17 +0530

    Update TrackController.java

--- src/main/java/com/stackroute/controller/TrackController.java
+++ src/main/java/com/stackroute/controller/TrackController.java
@@ -19,7 +19,7 @@ public class TrackController {
         this.trackService = trackService;
     }
 
-    @PostMapping("track")
+    @PostMapping("tracks")
     public ResponseEntity<?> saveTrack(@RequestBody Track track) {
 
         ResponseEntity responseEntity;
@@ -36,7 +36,7 @@ public class TrackController {
 
     }
 
-    @DeleteMapping(value = "/delete/{id}")
+    @DeleteMapping(value = "/track/{id}")
     public ResponseEntity<?> deleteTrack(@PathVariable Integer id) {
 
         ResponseEntity responseEntity;
@@ -54,7 +54,7 @@ public class TrackController {
         return responseEntity;
     }
 
-    @PutMapping(value = "/update/{id}/{comment}")
+    @PutMapping(value = "/track/{id}/{comment}")
     public ResponseEntity<?> updateTrack(@PathVariable int id, @PathVariable String comment) {
 
         ResponseEntity responseEntity;
